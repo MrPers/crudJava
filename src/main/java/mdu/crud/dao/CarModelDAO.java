a81f4bbbc7104785b1df464ffe7db7ec9e70e65b
MrPers <iamanton45@gmail.com>
2021-04-25 10:38:36 +0300

    Interface access options

---
+++ src/main/java/mdu/crud/dao/CarModelDAO.java
@@ -0,0 +1,9 @@
+package mdu.crud.dao;
+import mdu.crud.model.CarModel;
+import java.util.List;
+public interface CarModelDAO {
+    void saveOrUpdate(CarModel item);
+    void delete(int itemId);
+    CarModel get(int itemId);
+    List<CarModel> list();
+}
